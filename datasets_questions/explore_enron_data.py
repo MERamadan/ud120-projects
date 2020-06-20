becd1a52df01cde02ec0ad35485f65e58ce9c9b1
Mohamed Ramadan <mohamed.e.ramadan@gmail.com>
2020-06-20 22:01:31 +0200

    Regression

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -17,6 +17,22 @@
 
 import pickle
 
-enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
+enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r+"))
 
+total =  len(enron_data)
 
+poi_total = sum( x["poi"] == True for x in enron_data.values() )
+
+#print enron_data["PRENTICE JAMES"]["total_stock_value"]
+
+#print enron_data["COLWELL WESLEY"]["from_this_person_to_poi"]
+
+#print enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]
+
+NaN_total_payments = sum( p["total_payments"] == 'NaN' for p in enron_data.values())
+
+print total, NaN_total_payments, 1.00 * NaN_total_payments/total
+
+POI_NaN_total_payments = sum( p["poi"] == True and p["total_payments"] == 'NaN' for p in enron_data.values())
+
+print poi_total, POI_NaN_total_payments, 1.00 * POI_NaN_total_payments/poi_total
